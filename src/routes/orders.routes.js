2a437667c718706b09d1409a123ae1bef595204f
Eduardo Corgniali <educorgniali@gmail.com>
2021-10-30 16:52:02 -0300

    agregado documentacion API Routes Ordenes.

--- src/routes/orders.routes.js
+++ src/routes/orders.routes.js
@@ -5,6 +5,64 @@ const ordersController = require('../app/controllers/orders');
 const { loggerError } = require('../config/log4js');
 const { enviarMailOrdenGenerada } = require('../app/helpers/sendMail');
 
+
+/**
+ * @swagger
+ * tags:
+ *   name: Ordenes
+ */
+/**
+ * @openapi
+ * /ordenes/listar:
+ *   get:
+ *     summary: 'Listar todas las ordenes'
+ *     description: Devuelve un arreglo de todas las ordenes de compra del cliente autenticado
+ *     tags: [Ordenes]
+ *     responses:
+ *       200:
+ *         description: operación exitosa
+ *         schema:
+ *           type: array
+ *           items: 
+ *             type: object
+ *             properties:
+ *               estado:
+ *                 type: string
+ *                 descripcion: Estado de la orden de compra
+ *                 example: 'generada'
+ *               productos:
+ *                 type: array
+ *                 descripcion: Arreglo de los productos que tiene la orden
+ *                 items:
+ *                   type: object
+ *                   properties:
+ *                     codigo:
+ *                       type: string
+ *                     nombre:
+ *                       type: string
+ *                     descripcion:
+ *                       type: string
+ *                     precio:
+ *                       type: number
+ *                     foto:
+ *                       type: string
+ *                     cantidad:
+ *                       type: integer
+ *               email:
+ *                 type: string
+ *                 descripcion: Email del cliente
+ *                 example: 'username@correo.com'
+ *               direccion:
+ *                 type: string
+ *                 descripcion: Direccion de envio
+ *                 example: 'Av. siempre viva 742'
+ *               timestamp:
+ *                 type: string
+ *                 descripcion: Fecha y hs de creacion de la orden
+ *                 example: '30/10/2021 - 16:42:00HS'
+ *       403:
+ *         description: operacion no autorizada
+ */
 router.get('/listar', async (req, res) => {
     try {
         let ordenes = await ordersController.list({ email: req.user.email });
@@ -15,6 +73,24 @@ router.get('/listar', async (req, res) => {
     }
 });
 
+
+/**
+ * @openapi
+ * /ordenes/listar/{id}:
+ *   get:
+ *     summary: 'Listar una orden por su ID'
+ *     description: Devuelve un objeto con los datos de la orden correspondiente al ID pasado como parametro
+ *     tags: [Ordenes]
+ *     parameters:
+ *     - name: id
+ *       in: 'path'
+ *       description: 'ID de la orden a retornar'
+ *     responses:
+ *       200:
+ *         description: operación exitosa
+ *       403:
+ *         description: operacion no autorizada
+ */
 router.get('/listar/:id', async (req, res) => {
     try {
         let orden = await ordersController.listId(req.params.id);
@@ -25,6 +101,20 @@ router.get('/listar/:id', async (req, res) => {
     }
 });
 
+
+/**
+ * @openapi
+ * /ordenes/agregar:
+ *   post:
+ *     summary: 'Agregar una nueva orden'
+ *     description: Genera una nueva orden con los productos que tiene cargados el carrito del cliente y se envia un aviso de email al admin con el detalle del pedido. Si el carrito esta vacío arroja un mensaje de error. 
+ *     tags: [Ordenes]
+ *     responses:
+ *       200:
+ *         description: operación exitosa
+ *       403:
+ *         description: operacion no autorizada
+ */
 router.post('/agregar', async (req, res) => {
     try {
         let cliente = {
@@ -41,15 +131,39 @@ router.post('/agregar', async (req, res) => {
             }
             throw new Error('Error al guardar orden')
         } else {
-            res.status(404).json({ error: 'Antes de generar un pedido debe agregar productos al carrito' });
+            res.json({ error: 'Antes de generar un pedido debe agregar productos al carrito' });
         }
     } catch (error) {
         loggerError.error(error.message);
         res.json({ error: 'La orden no pudo ser generada' })
     }
-
 });
 
+
+/**
+ * @openapi
+ * /ordenes/actualizar/{id}:
+ *   put:
+ *     summary: 'Actualizar una orden'
+ *     description: Actualiza la orden correspondiente al ID pasado como parametro
+ *     tags: [Ordenes]
+ *     parameters:
+ *     - name: 'id'
+ *       in: 'path'
+ *       description: 'ID de la orden actualizar'
+ *     - name: 'body'
+ *       in: 'body'
+ *       description: 'Atributos a modificar'
+ *       schema:
+ *         type: object
+ *         properties:
+ *           direccion:
+ *             type: string
+ *             example: 'Nueva direccion de envio'
+ *     responses:
+ *       200:
+ *         description: operacion exitosa
+ */
 router.put('/actualizar/:id', async (req, res) => {
     try {
         res.json(await ordersController.update(req.params.id, req.body));
@@ -59,6 +173,22 @@ router.put('/actualizar/:id', async (req, res) => {
     }
 });
 
+
+/**
+ * @openapi
+ * /ordenes/borrar/{id}:
+ *   delete:
+ *     summary: 'Eliminar orden'
+ *     description: Elimina la orden correspondiente al ID pasado como parametro
+ *     tags: [Ordenes]
+ *     parameters:
+ *     - name: 'id'
+ *       in: 'path'
+ *       description: 'ID de la orden a eliminar'
+ *     responses:
+ *       200:
+ *         description: operacion exitosa
+ */
 router.delete('/borrar/:id', async (req, res) => {
     try {
         res.json(await ordersController.delete(req.params.id));
@@ -68,6 +198,31 @@ router.delete('/borrar/:id', async (req, res) => {
     }
 });
 
+
+/**
+ * @openapi
+ * /ordenes/confirmar/{id}:
+ *   put:
+ *     summary: 'Confirmar una orden'
+ *     description: Comfirma la orden correspondiente al ID pasado como parametro y la pasa a estado enviada.
+ *     tags: [Ordenes]
+ *     parameters:
+ *     - name: 'id'
+ *       in: 'path'
+ *       description: 'ID de la orden actualizar'
+ *     - name: 'body'
+ *       in: 'body'
+ *       description: 'Atributos a modificar'
+ *       schema:
+ *         type: object
+ *         properties:
+ *           estado:
+ *             type: string
+ *             example: 'enviada'
+ *     responses:
+ *       200:
+ *         description: operacion exitosa
+ */
 router.put('/confirmar/:id', async (req, res) => {
     try {
         res.json(await ordersController.update(req.params.id, { estado: 'enviada' }));
