d41e6d6f7918ebc9f4428323a87eb88ce9eda746
Eduardo Corgniali <educorgniali@gmail.com>
2021-11-02 18:20:24 -0300

    agregada DAO/persistencia fileSystem para ordenes de compra

--- src/app/DAO/orders/fileSystem.js
+++ src/app/DAO/orders/fileSystem.js
@@ -1,7 +1,6 @@
 const IDao = require('../IDao');
 const fs = require('fs');
 const { v4: uuidv4 } = require('uuid');
-const fsDaoProducts = require('../products/fileSystem').getInstance();
 
 let instaciaFileSystem = null;
 
@@ -19,6 +18,68 @@ class FileSystemDao extends IDao {
         }
 
         return instaciaFileSystem;
+    }
+
+    create(cliente, carrito) {
+        let ordenes = JSON.parse(fs.readFileSync(this.urlPath, 'utf-8'));        
+        let nuevaOrden = {
+            id: uuidv4(),
+            estado: 'generada',
+            productos: carrito.map(e => {
+                return {
+                    id: uuidv4(),
+                    codigo: e.producto.codigo,
+                    nombre: e.producto.nombre,
+                    descripcion: e.producto.descripcion,
+                    precio: e.producto.precio,
+                    foto: e.producto.foto,
+                    cantidad: e.cantidad
+                }
+            }),
+            email: cliente.email,
+            direccion: cliente.direccion,
+            timestamp: new Date().toLocaleString()
+        } 
+        ordenes.push(nuevaOrden);
+        fs.writeFileSync(this.urlPath, JSON.stringify(ordenes, null, '\t'));
+        return nuevaOrden;
+    }
+
+    read(query) {
+        let ordenes = JSON.parse(fs.readFileSync(this.urlPath, 'utf-8'));
+        let ordenesCliente = ordenes.filter(e => e.email == query.email);
+        return ordenesCliente;
+    }
+
+    readId(id) {
+        let ordenes = JSON.parse(fs.readFileSync(this.urlPath, 'utf-8'));
+        let orden = ordenes.filter(e => e.id == id);
+        return orden[0];
+    }
+
+    update(id, data) {
+        let ordenes = JSON.parse(fs.readFileSync(this.urlPath, 'utf-8'));
+        let orden = ordenes.filter(p => p.id == id);
+        if (orden.length) {
+            let ordenActualizada = Object.assign(orden[0], data);
+            ordenActualizada.timestamp = new Date().toLocaleString();
+            fs.writeFileSync(this.urlPath, JSON.stringify(ordenes, null, '\t'));
+            return ordenActualizada;
+        } else {
+            return false;
+        }
+    }
+
+    delete(id) {
+        let ordenes = JSON.parse(fs.readFileSync(this.urlPath, 'utf-8'));
+        let index = ordenes.findIndex(e => e.id == id);
+        if (index >= 0) {
+            const ordenEliminada = ordenes.splice(index, 1);
+            fs.writeFileSync(this.urlPath, JSON.stringify(ordenes, null, '\t'));
+            return ordenEliminada[0];
+        } else {
+            return false;
+        }
     }
 
 }
