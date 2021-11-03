3e5f9ad1b71a0a50f6b002834da2ca17f6e6fcba
Eduardo Corgniali <educorgniali@gmail.com>
2021-11-03 15:48:38 -0300

    agregado al atributo email modelo user mongo config lowercase

--- src/app/models/mongoose/user.js
+++ src/app/models/mongoose/user.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const schema = mongoose.Schema({
-    email: { type: String, required: true, unique: true },
+    email: { type: String, required: true, lowercase: true, unique: true },
     password: { type: String, required: true },
     nombre: { type: String, required: true },
     direccion: { type: String, required: false },
